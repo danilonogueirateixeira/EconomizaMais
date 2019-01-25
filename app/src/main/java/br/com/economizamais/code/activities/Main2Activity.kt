aec6a0f08947339ffbeb4f8280f020c388a90b6b
Danilo <danilo_dnt@outlook.com>
2019-01-24 23:30:10 -0200

    Conexão com servidor através do Retrofit

--- app/src/main/java/br/com/economizamais/code/activities/Main2Activity.kt
+++ app/src/main/java/br/com/economizamais/code/activities/Main2Activity.kt
@@ -8,12 +8,19 @@ import android.support.v7.app.AppCompatActivity
 import br.com.economizamais.R
 import kotlinx.android.synthetic.main.activity_main2.*
 import android.util.Log
+import android.widget.Toast
 import br.com.economizamais.code.DataBase.ProdutoDatabase
 import br.com.economizamais.code.dao.DeleteAllProdutoTask
 import br.com.economizamais.code.dao.GetAllProdutoTask
 import br.com.economizamais.code.dao.InsertProdutoTask
 import br.com.economizamais.code.dao.ProdutoDao
 import br.com.economizamais.code.entities.Produto
+import br.com.economizamais.code.network_utils.Endpoint
+import br.com.economizamais.code.network_utils.NetworkUtils
+import br.com.economizamais.code.network_utils.ProdutoServer
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 
 
 class Main2Activity : AppCompatActivity() {
@@ -62,8 +69,41 @@ class Main2Activity : AppCompatActivity() {
 
         GetAllProdutoTask(database).execute()
 
+        getData()
 
     }
+
+
+    fun getData() {
+        val retrofitClient = NetworkUtils
+            .getRetrofitInstance("https://economizamais.herokuapp.com/")
+
+        Log.i("TESTE ----->", "TESTE")
+
+        val endpoint = retrofitClient.create(Endpoint::class.java)
+        val callback = endpoint.getPosts()
+
+        callback.enqueue(object : Callback<List<br.com.economizamais.code.network_utils.ProdutoServer>> {
+
+
+            override fun onFailure(call: Call<List<br.com.economizamais.code.network_utils.ProdutoServer>>, t: Throwable) {
+                Toast.makeText(baseContext, t.message, Toast.LENGTH_SHORT).show()
+                Log.i("TESTE ----->",t.message)
+
+            }
+
+
+            override fun onResponse(call: Call<List<br.com.economizamais.code.network_utils.ProdutoServer>>, response: Response<List<br.com.economizamais.code.network_utils.ProdutoServer>>) {
+                response.body()?.forEach {
+                    //Toast.makeText(baseContext, it.id.toString(), Toast.LENGTH_SHORT).show()
+                    Log.i("TESTE ----->",  it.toString())
+
+                }
+            }
+        })
+
+    }
+
 }
 
 
